930ef66bbabc340765cbe59e5655e59228b6ad1d
Aryan Sharma <aryan.co19@nsut.ac.in>
2021-08-02 08:29:55 +0530

    added comments

--- Competative/Graphs/countingRooms.cpp
+++ Competative/Graphs/countingRooms.cpp
@@ -1,30 +1,33 @@
 #include<bits/stdc++.h>
 #pragma GCC diagnostic ignored "-Wc++11-extensions"
 using namespace std;
-//Test to check for github
+
 int n,m,rooms;
-vector<vector<bool> > visited;
+vector<vector<bool> > vis;
+vector<pair<int,int> > moves;
+
 bool isValid(int x,int y){
-    if(x<0 || x>=n || y<0 || y>=m) return false;
-    if(visited[x][y]) return false;
-    return true;
+    if(x<0 || x>=n || y<0 || y>=m) return false; // Check out of bound error
+    if(vis[x][y]) return false; // Check if ever visited or not
+    return true; // If not visited or not out of bound, return true
 }
 
-void dfs(int i,int j,vector<pair<int,int> > moves){
-    visited[i][j]=true;
-    for(auto p: moves){
-        if(isValid(i+p.first,j+p.second)){
-            dfs(i+p.first,j+p.second,moves);
+void dfs(int i,int j){
+    vis[i][j]=true; // Mark the point as visited so that repetation wont be there
+    for(auto p: moves){ // To check for all movement around
+    //isValid function checks if x,y co-ordinate is not going out of bounds or if it visited or not. If visited or out of bound, it returns false
+        if(isValid(i+p.first,j+p.second)){ // p.first is first component of pair and p.second is second one (like (0,1), (-1,0) etc)
+            dfs(i+p.first,j+p.second); // Do dfs from here
         }
     }
 }
 
-void connectedComponents(vector<pair<int,int> > moves){
+void connectedComponents(){ // This function is to check for different rooms
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(!visited[i][j]){
-                dfs(i,j,moves);
-                ++rooms;
+            if(!vis[i][j]){
+                dfs(i,j); // We will check for the whole room
+                ++rooms; // As soon as we come out of dfs, it will mean we have completed one room hence room will be ++
             }
         }
     }
@@ -34,11 +37,12 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
     cin>>n>>m;
-    visited.resize(n);
-    for(int i=0;i<n;i++) visited[i].resize(m);
-
-    vector<pair<int,int> > moves;
+    vis.resize(n);
+    for(int i=0;i<n;i++) vis[i].resize(m);
+    
+    // Moves are up (-1,0), down (1,0), left (0,-1), right(0,1)
     moves.push_back(make_pair(-1,0));
     moves.push_back(make_pair(1,0));
     moves.push_back(make_pair(0,-1));
@@ -46,13 +50,13 @@ int main()
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            char c; cin>>c;
+            char c;cin>>c;
             if(c=='#'){
-                visited[i][j]=true;
+                vis[i][j]=true; // Mark wall as true so that we can not visit them (rest all are false by default due to global variable)
             }
         }
     }
 
-    connectedComponents(moves);
+    connectedComponents();
     cout<<rooms;
 }
