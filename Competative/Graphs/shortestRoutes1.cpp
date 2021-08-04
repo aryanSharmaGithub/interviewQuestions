7ae678cfe7ad7c7b4da0501fcb33c4ed780235ef
Aryan Sharma <aryan.co19@nsut.ac.in>
2021-08-04 12:22:31 +0530

    try2

--- Competative/Graphs/shortestRoutes1.cpp
+++ Competative/Graphs/shortestRoutes1.cpp
@@ -1,33 +1,34 @@
 #include<bits/stdc++.h>
+#pragma GCC diagnostic ignored "-Wc++11-extensions"
 using namespace std;
 
-#pragma GCC diagnostic ignored "-Wc++11-extensions"
-#define end "\n"
+#define mp(x,y) make_pair(x,y)
 #define int long long int
 #define pii pair<int,int>
 
 int n,m;
-vector<vector<pii> >g;
-vector<int>dist;
+vector<int> dist;
+vector<vector<pii> > g;
 
 void dijkastra(){
-    for(int i=0;i<=n;i++) dist[i]=LONG_MAX;
+    for(int i=1;i<n+1;i++) dist[i]=LONG_MAX;
     dist[1]=0;
-    priority_queue<pii,vector<pii>,greater<pii> > pq;
-    pq.push(make_pair(0,1));
+    priority_queue<pii,vector<pii>,greater<pii> >p;
+    p.push(mp(0,1)); // x = distance, y = vertice
 
-    while(!pq.empty()){
-        int dis = pq.top().first;
-        int u = pq.top().second;
-        pq.pop();
+    while(!p.empty()){
+        int dis = p.top().first;
+        int u = p.top().second;
+        p.pop();
+
         if(dist[u]<dis) continue;
         for(auto e: g[u]){
             int v = e.first;
             int c = e.second;
             if(dist[v]<=dis+c) continue;
-            if(dist[v]>dis+c){
+            else{
                 dist[v]=dis+c;
-                pq.push(make_pair(dist[v],v));
+                p.push(mp(dist[v],v));
             }
         }
     }
@@ -35,18 +36,16 @@ void dijkastra(){
 
 int32_t main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     cin>>n>>m;
-    g.resize(n+1);
     dist.resize(n+1);
-    for(auto i=0;i<m;i++){
+    g.resize(n+1);
+    for(int i=0;i<m;i++){
         int u,v,c;
         cin>>u>>v>>c;
-        g[u].push_back(make_pair(v,c));
+        g[u].push_back(mp(v,c));
     }
+
     dijkastra();
+
     for(int i=1;i<=n;i++) cout<<dist[i]<<" ";
 }
